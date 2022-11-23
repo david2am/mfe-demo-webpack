9c6419cdd2aabe49a788dea25826d65f32ad39ea
CodeSandbox <support@codesandbox.io>
2022-11-23 15:07:08 +0000

    Initial commit

---
+++ host/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from "remote/Header";
+</script>
+
+<template>
+  <div class="container">
+    <Header />
+    <div>Name: host</div>
+    <div>Framework: vue3</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+</template>
